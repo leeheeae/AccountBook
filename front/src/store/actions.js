5c1a03ab95d5693180e34dd5761e980fb058aa35
leeheeae <chromeee@daum.net>
2021-12-16 13:50:13 +0900

    async await 변환 연습

--- front/src/store/actions.js
+++ front/src/store/actions.js
@@ -8,17 +8,23 @@ import {
 
 export default {
 	FETCH_DATE_LIST({ commit }) {
-		fetchDateList()
+		return fetchDateList()
 			.then(({ data }) => {
 				commit('SET_DATE_LIST', data);
 			})
 			.catch((err) => console.error(err));
 	},
-	FETCH_WHERE_DATE_LIST({ commit }, payload) {
-		fetchWhereDateList(payload)
-			.then(({ data }) => {
-				commit('SET_WHERE_DATE_LIST', data);
-			})
-			.catch((err) => console.error(err));
+	// FETCH_WHERE_DATE_LIST({ commit }, payload) {
+	// 	return  fetchWhereDateList(payload)
+	// 		.then(({ data }) => {
+	// 			commit('SET_WHERE_DATE_LIST', data);
+	// 		})
+	// 		.catch((err) => console.error(err));
+	// },
+
+	async FETCH_WHERE_DATE_LIST({ commit }, payload) {
+		const response = await fetchWhereDateList(payload);
+		commit('SET_WHERE_DATE_LIST', response.data);
+		return response;
 	},
 };
